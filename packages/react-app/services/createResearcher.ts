0035562395b0464668f5a511f5cae4ccf16bec23
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-07-08 19:10:01 +0300

    Add initial services

---
+++ packages/react-app/services/createResearcher.ts
@@ -0,0 +1,69 @@
+import { canvassingContractABI } from "@/utils/abis/canvassingContractABI";
+import { canvassingContractAddress } from "@/utils/addresses/canvassingContractAddress";
+import {
+  createPublicClient,
+  createWalletClient,
+  custom,
+  http,
+  parseTransaction,
+} from "viem";
+import { celoAlfajores } from "viem/chains";
+
+export const createEvent = async (
+  _signerAddress: `0x${string}` | undefined,
+  {
+    _walletAddress,
+    _industry,
+    _numberOfEmployees,
+    _yearsInOperation,
+  }: CreateResearcherProps
+): Promise<boolean> => {
+  if (window.ethereum) {
+    const privateClient = createWalletClient({
+      chain: celoAlfajores,
+      transport: custom(window.ethereum),
+    });
+    const publicClient = createPublicClient({
+      chain: celoAlfajores,
+      transport: custom(window.ethereum),
+    });
+    const [address] = await privateClient.getAddresses();
+    try {
+      const createResearcherEventTxn = await privateClient.writeContract({
+        account: address,
+        address: canvassingContractAddress,
+        abi: canvassingContractABI,
+        functionName: "createResearcher",
+        args: [
+          _walletAddress,
+          _industry,
+          _numberOfEmployees,
+          _yearsInOperation,
+        ],
+      });
+
+      const createResearcherEventTxnReceipt =
+        await publicClient.waitForTransactionReceipt({
+          hash: createResearcherEventTxn,
+        });
+
+      if (createResearcherEventTxnReceipt.status == "success") {
+        return true;
+      } else {
+        return false;
+      }
+
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+  return false;
+};
+
+export type CreateResearcherProps = {
+  _walletAddress: `0x${string}`;
+  _industry: string;
+  _numberOfEmployees: string;
+  _yearsInOperation: number;
+};
