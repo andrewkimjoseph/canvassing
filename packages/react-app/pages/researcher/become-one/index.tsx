559e5329c29e2196a7579b4f6f5821c63dd0acda
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-07-08 23:16:33 +0300

    Update researcher

---
+++ packages/react-app/pages/researcher/become-one/index.tsx
@@ -0,0 +1,191 @@
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import router from "next/router";
+import { createResearcher } from "@/services/createResearcher";
+export default function ResearcherAccountCreation() {
+  const [userAddress, setUserAddress] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const { address, isConnected } = useAccount();
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [industry, setIndustry] = useState("Finance");
+  const [numberOfEmployees, setNumberOfEmployees] = useState("10 +");
+  const [yearsInOperation, setYearsInOperation] = useState("5 - 10");
+  const [creatingResearcher, setIsCreatingResearch] = useState(false);
+
+  const createResearcherAccount = async () => {
+    setIsCreatingResearch(true);
+
+    const researcherIsCreated = await createResearcher(address, {
+      _walletAddress: address as `0x${string}`,
+      _industry: industry,
+      _numberOfEmployees: numberOfEmployees,
+      _yearsInOperation: yearsInOperation,
+    });
+
+    if (researcherIsCreated) {
+    } else {
+    }
+    setIsCreatingResearch(false);
+  };
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isConnected && address) {
+      setUserAddress(address);
+    }
+  }, [address, isConnected]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col items-left py-2 mx-4 h-svh relative">
+      <Text fontSize={"14"} align={"left"} onClick={() => router.back()}>
+        Go back
+      </Text>
+      <Text fontWeight={"bold"} fontSize={"20"} mt={2} mb={1}>
+        Become a Researcher
+      </Text>
+      <Image
+        // paddingY={8}
+        height={"150px"}
+        width={"150px"}
+        src="/researcher.png"
+        alt="Home image"
+      />
+  
+
+      <Text  fontSize={"16"} mt={1} mb={1}>
+        Select Details
+      </Text>
+
+      <Stack spacing={1}>
+        <Text fontSize={"12"} mb={1}>
+          Industry
+        </Text>
+        <Select
+          bgColor={"#F5E8C7"}
+          value={industry}
+          onChange={(event) => {
+            setIndustry(event.target.value);
+          }}
+        >
+          <option value="Finance">Finance</option>
+          <option value="Non-finance">Non-finance</option>
+        </Select>
+
+        <Text fontSize={"12"} mb={1}>
+          Number of Employees
+        </Text>
+        <Select
+          bgColor={"#F5E8C7"}
+          value={numberOfEmployees}
+          onChange={(event) => {
+            setNumberOfEmployees(event.target.value);
+          }}
+        >
+          <option value="0 - 5">0 - 5</option>
+          <option value="5 - 10">5 - 10</option>
+          <option value="10 +">10 +</option>
+        </Select>
+
+        <Text fontSize={"12"} mb={1}>
+          Years in Operation
+        </Text>
+        <Select
+          bgColor={"#F5E8C7"}
+          value={yearsInOperation}
+          onChange={(event) => {
+            setYearsInOperation(event.target.value);
+          }}
+        >
+          <option value="0 - 5">0 - 5</option>
+          <option value="5 - 10">5 - 10 </option>
+          <option value="10 +">10 +</option>
+        </Select>
+      </Stack>
+
+      {/* <Button onClick={onOpen}>Open Modal</Button> */}
+
+      <Modal
+        blockScrollOnMount={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        
+      >
+        <ModalOverlay />
+        <ModalContent mx={16} borderRadius={10}>
+          <ModalHeader>Modal Title</ModalHeader>
+          {/* <ModalCloseButton /> */}
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              bgColor={"black"}
+              color={"white"}
+              onClick={onClose}
+              width={"full"}
+              borderRadius={10}
+            >
+              Close
+            </Button>
+            {/* <Button variant='ghost'>Secondary Action</Button> */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Button
+        onClick={createResearcherAccount}
+        isLoading={creatingResearcher}
+        mb={24}
+        bottom={0}
+        position={"absolute"}
+        // marginTop={"4"}
+        loadingText="Creating"
+        borderRadius={"10"}
+        width={"full"}
+        bgColor={"#363062"}
+        textColor={"white"}
+        _hover={{
+          bgColor: "#F5E8C7",
+          textColor: "black",
+        }}
+      >
+        Create your researcher account
+      </Button>
+    </div>
+  );
+}
+
+export type ResearchAccountCreationErrors = {
+  category: "industry" | "numberOfEmployees" | "yearsInOperation";
+  isInvalid: boolean;
+};
---
+++ packages/react-app/pages/researcher/index.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { Button, Image, Text } from "@chakra-ui/react";
+export default function ResearcherHome() {
+  const [userAddress, setUserAddress] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isConnected && address) {
+      setUserAddress(address);
+    }
+  }, [address, isConnected]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center py-2 h-full">
+      <Image
+        // paddingY={8}
+        height={"300px"}
+        src="/home.png"
+        alt="Home image"
+      />
+      <Text fontWeight={"bold"} fontSize={"18"}>
+        Welcome to Canvassing!
+      </Text>
+      <Text fontSize={"14"}>opinions pay, today</Text>
+
+      <Text fontSize={"18"} marginTop={10} marginBottom={4}>
+        Continue as ...
+      </Text>
+
+      <Button
+        //   onClick={() => router.push("/")}
+
+        width={"95%"}
+        borderRadius={"10"}
+        bgColor={"#F5E8C7"}
+        textColor={"black"}
+      >
+        Participant
+      </Button>
+
+      <Button
+        //   onClick={() => router.push("/")}
+
+        marginTop={"4"}
+        borderRadius={"10"}
+        width={"95%"}
+        bgColor={"#363062"}
+        textColor={"white"}
+      >
+        Researcher
+      </Button>
+    </div>
+  );
+}
